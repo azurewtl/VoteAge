5a2419e079f46a64126fa762c2fb727a2d0ef1ca
azure <azure@azures-MacBook-Air.local>
2014-11-06 15:51:32 +0800

    Initial Commit

---
+++ VoteAgeTests/VoteAgeTests.swift
@@ -0,0 +1,36 @@
+//
+//  VoteAgeTests.swift
+//  VoteAgeTests
+//
+//  Created by azure on 14/11/6.
+//  Copyright (c) 2014年 azure. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class VoteAgeTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
